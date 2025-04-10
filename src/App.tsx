9543d6a2fead3ab878f89620b8399569afd19b4a
Klonochkin <di630538@yandex.ru>
2025-04-10 11:31:19 +0300

    feat: Добавлять backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from 'react';
+
+interface ApiResponse {
+    message: string;
+}
+
 export default function App() {
-    return <div className='text-center'>Пока</div>;
+    const [data, setData] = useState<ApiResponse | null>(null);
+
+    useEffect(() => {
+        fetch('http://localhost:8000/')
+            .then((response) => response.json())
+            .then((result: ApiResponse) => setData(result));
+    }, []);
+    return (
+        <div className='text-center'>
+            Пока
+            <pre>{JSON.stringify(data?.message, null, 2)}</pre>
+        </div>
+    );
 }
