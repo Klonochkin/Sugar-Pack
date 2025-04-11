491df855e475cd05cd1cdbee9ed1b1877b03e3f0
Klonochkin <di630538@yandex.ru>
2025-04-11 17:09:06 +0300

    feat: Добавляет header

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,18 @@ function Control() {
 
     return (
         <div>
+            <div className='fixed w-screen top-0 left-0 h-[5rem] bg-[#141414]'>
+                <div className='flex flex-row items-center justify-center h-full w-full place-content-between gap-[5rem]'>
+                    <img
+                        className='cursor-pointer w-[8rem]'
+                        src='../images/logo.png'
+                        alt=''
+                    />
+                    <p className='cursor-pointer'>+7 (495) 246-20-20</p>
+                    <p className='cursor-pointer'>+7 (495) 536-65-10</p>
+                    <p className='cursor-pointer'>info@sahara-pack.ru</p>
+                </div>
+            </div>
             <div className='flex gap-4 flex-col'>
                 {currentPage >= 0 && currentPage <= 1 && <AuthForm />}
             </div>
@@ -29,7 +41,7 @@ export default function App() {
     }, []);
     return (
         <CurrentPageContext.Provider value={{ currentPage, setCurrentPage }}>
-            <main className='max-w-[750px] m-auto antialiased'>
+            <main className='max-w-[750px] m-auto antialiased relative'>
                 <Control />
             </main>
         </CurrentPageContext.Provider>
